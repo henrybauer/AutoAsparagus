3bc9533ae839a0fafcb2f81d9ceecfbb526f2fbe
Henry Bauer <github@henrybauer.com>
2014-08-27 12:29:29 +0300

    Update install checker. Don't forget to add the "System" assembly reference, sigh.

--- AutoAsparagus/ASPInstallChecker.cs
+++ AutoAsparagus/ASPInstallChecker.cs
@@ -1,4 +1,4 @@
-﻿/**
+/*
  * Originally based on the InstallChecker from the Kethane mod for Kerbal Space Program.
  * This version is based off taraniselsu's version from https://github.com/Majiir/Kethane/blob/b93b1171ec42b4be6c44b257ad31c7efd7ea1702/Plugin/InstallChecker.cs
  * 
@@ -24,14 +24,18 @@ namespace AutoAsparagus
 		{
 			const string modName = "AutoAsparagus";
 			const string expectedPath = "AutoAsparagus";
-
 			// Search for this mod's DLL existing in the wrong location. This will also detect duplicate copies because only one can be in the right place.
-			var assemblies = AssemblyLoader.loadedAssemblies.Where(a => a.assembly.GetName().Name == Assembly.GetExecutingAssembly().GetName().Name).Where(a => a.url != expectedPath);
+			var assemblies = AssemblyLoader.loadedAssemblies.Where (a => a.assembly.GetName ().Name == Assembly.GetExecutingAssembly ().GetName ().Name).Where (a => a.url != expectedPath);
+
 			if (assemblies.Any()) {
-				var badPaths = assemblies.Select(a => a.path).Select(p => Uri.UnescapeDataString(new Uri(Path.GetFullPath(KSPUtil.ApplicationRootPath)).MakeRelativeUri(new Uri(p)).ToString().Replace('/', Path.DirectorySeparatorChar)));
+				var badPaths = assemblies.Select(a => a.path).Select(
+					p => Uri.UnescapeDataString(new Uri(Path.GetFullPath(KSPUtil.ApplicationRootPath)).MakeRelativeUri(new Uri(p)).ToString().Replace('/', Path.DirectorySeparatorChar))
+				);
 				PopupDialog.SpawnPopupDialog("Incorrect " + modName + " Installation",
-				modName + " has been installed incorrectly and will not function properly. All files should be located in KSP/GameData/" + expectedPath + ". Do not move any files from inside that folder.\n\nIncorrect path(s):\n" + String.Join("\n", badPaths.ToArray()),
-				"OK", false, HighLogic.Skin);
+				    modName + " has been installed incorrectly and will not function properly. All files should be located in KSP/GameData/" + 
+					expectedPath + ". Do not move any files from inside that folder.\n\nIncorrect path(s):\n" +
+					String.Join("\n", badPaths.ToArray()),
+					"OK", false, HighLogic.Skin);
 			}
 		}
 	}
